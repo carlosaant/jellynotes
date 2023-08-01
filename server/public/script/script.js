7fa098665e90787916b1ef54db0cf13e02ca1fd8
Antonio Carlos <carlos_antonio.minga@outlook.com>
2023-08-01 16:03:18 -0300

    estilização de exibiçao e funçao para newpost

--- server/public/script/script.js
+++ server/public/script/script.js
@@ -24,7 +24,7 @@ function updatePosts() {
       let posts = JSON.parse(json);
       let postsElement = "";
       posts.forEach((post) => {
-        const cardPost = `<div id=${post.id} class="card col col-sm-3 m-1">
+        const cardPost = `<div id=${post.id} class="card col col-sm-5 my-2">
         <div class="card-header">
           <h5 class="card-title">${post.title}</h5>
         </div>
@@ -42,5 +42,24 @@ function updatePosts() {
 }
 
 function newPost() {
-  console.log("clicou para enviar");
+  if (
+    document.getElementById("post_title").value !== "" &&
+    document.getElementById("post_desc").value !== ""
+  ) {
+    let title = document.getElementById("post_title").value;
+    let description = document.getElementById("post_desc").value;
+    let post = { title, description };
+    console.log(post);
+    const options = {
+      method: "POST",
+      headers: new Headers({ "content-type": "application/json" }),
+      body: JSON.stringify(post),
+    };
+
+    fetch(appURL + "/api/new", options)
+      .then((res) => {
+        console.log(res);
+      })
+      .then(updatePosts());
+  }
 }
